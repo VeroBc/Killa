43ae037928dc2c2889ab81d4c2bc45abf8008196
VeroBc <verobc.cel@gmail.com>
2020-03-23 15:09:40 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const sgMail = require('@sendgrid/mail');
+
+const app = express()
+app.use(express.urlencoded({ extended: false }));
+
+const puerto = 7777
+
+const msg = {
+    to: 'verobc.cel@gmail.com',
+    from: 'verobc.cel@gmail.com'
+};
+
+app.get('/enviarEmail', (req, res) => {
+
+    sgMail.setApiKey('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');
+    let aMsg = msg;
+    aMsg.subject ='Enviando correos con SendGrid is Fun'
+    aMsg.text = 'Esta es una prueba para enviar correos'
+    aMsg.html = '<strong>Esta es una prueba para enviar correos</strong>'
+
+    sgMail
+        .send(aMsg)
+        .then(() => {console.log('correo enviado')}, console.error);
+    res.send('Aquí vamos a mandar un email')
+});
+
+app.get('/llenarForm',  (req, res) => {
+    res.sendFile( __dirname + '/HTML/userForm1.html');
+});
+
+app.post('/recibirData',  (req, res) => {
+
+    let mensaje = "Gracias por enviar la información: ";
+    mensaje += " Nombre: " +req.body.fname + " " + req.body.lname + "."
+    mensaje += " DNI: "+ req.body.dni
+    mensaje += ". Género: " + req.body.gender 
+    mensaje += ". Ciudad: " + req.body.ciudad
+
+    if ( req.body.hobbie1 != null) 
+        mensaje += ". Hobbie 1: " + req.body.hobbie1
+
+    if ( req.body.hobbie2 != null)    
+        mensaje += ". Hobbie 2: " + req.body.hobbie2
+
+    if ( req.body.hobbie3 != null)
+        mensaje += ". Hobbie 3: " + req.body.hobbie3
+
+    res.send(mensaje);
+
+    sgMail.setApiKey('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');
+
+    let aMsg = msg;
+    aMsg.subject ='Recibir data de formulario'
+    aMsg.text = mensaje
+    sgMail.send(aMsg)
+
+});
+
+
+app.listen(puerto, () => console.log(`Estamos empezando a escucharte por segunda vez ${puerto}!`))
+
